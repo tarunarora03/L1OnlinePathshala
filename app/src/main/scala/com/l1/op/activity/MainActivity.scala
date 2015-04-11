a8b3b95cadf7a6b4d730df93df0b148f73f06086
tarunarora <tarunarora03@gmail.com>
2015-04-11 19:52:09 -0400

    code refactoring to use scala features

--- app/src/main/scala/com/l1/op/activity/MainActivity.scala
+++ app/src/main/scala/com/l1/op/activity/MainActivity.scala
@@ -5,7 +5,6 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.view.View
-import android.view.View.OnClickListener
 import android.widget._
 import com.l1.op.R
 import com.l1.op.helper.DataBaseAdapter
@@ -14,7 +13,7 @@ import com.l1.op.util.{TR, TypedActivity}
 /**
  * Created by Tarun on 4/5/2015.
  */
-class MainActivity extends Activity with TypedActivity{
+class MainActivity extends Activity with TypedActivity {
 
   lazy val databaseAdapter = new DataBaseAdapter(this)
 
@@ -24,16 +23,10 @@ class MainActivity extends Activity with TypedActivity{
     val extras: Bundle = getIntent.getExtras
     val userName = extras.getString("username")
 
-    val clickHeader = findViewById(R.id.header).asInstanceOf[ImageView]
-    clickHeader.setOnClickListener(new OnClickListener {
-      override def onClick(v: View): Unit = {
-        val intentHeader: Intent = new Intent()
-        intentHeader.setAction(Intent.ACTION_VIEW)
-        intentHeader.addCategory(Intent.CATEGORY_BROWSABLE)
-        intentHeader.setData(Uri.parse("http://www.l1coaching.co.in"))
-        startActivity(intentHeader)
-      }
-    })
+    //Clickable Header Code
+    findViewById(R.id.header).asInstanceOf[ImageView].onClick {
+      view: View => startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.l1coaching.co.in")))
+    }
 
     val scores: Array[String] = databaseAdapter.fetchScores(userName)
     val userTextView = findView(TR.usernameR)
@@ -41,33 +34,30 @@ class MainActivity extends Activity with TypedActivity{
     userTextView.setText(userName)
     scoreView.setText(scores(0) + "/" + scores(1))
 
-    addListenerOnSubmit(userName,scores)
+    addListenerOnSubmit(userName, scores)
   }
 
   def addListenerOnSubmit(username: String, scores: Array[String]) = {
 
     val ansGroup = findView(TR.selectedAnsRadioGrp)
-    val btnSubmit = findView(TR.submitButton)
 
-    btnSubmit.setOnClickListener(new OnClickListener {
-      override def onClick(v: View): Unit = {
-        val selectedOpt = ansGroup.getCheckedRadioButtonId
-        val selAns = findViewById(selectedOpt).asInstanceOf[RadioButton]
-        val idx: Int = ansGroup.indexOfChild(selAns)
-        idx match {
-          case 1 => databaseAdapter.updateScore(username, (scores(0).toInt+1).toString, (scores(1).toInt+1).toString)
-          case _ => databaseAdapter.updateScore(username, scores(0).toInt.toString, (scores(1).toInt+1).toString)
-        }
-        val intent: Intent = getIntent
-        intent.putExtra("username", username)
-        intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
-        finish()
-        startActivity(intent)
+    findView(TR.submitButton).onClick { view: View =>
+      val selectedOpt = ansGroup.getCheckedRadioButtonId
+      val selAns = findViewById(selectedOpt).asInstanceOf[RadioButton]
+      val idx: Int = ansGroup.indexOfChild(selAns)
+      idx match {
+        case 1 => databaseAdapter.updateScore(username, (scores(0).toInt + 1).toString, (scores(1).toInt + 1).toString)
+        case _ => databaseAdapter.updateScore(username, scores(0).toInt.toString, (scores(1).toInt + 1).toString)
       }
-    })
+      val intent: Intent = getIntent
+      intent.putExtra("username", username)
+      intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+      finish()
+      startActivity(intent)
+    }
   }
 
-  override def onDestroy(): Unit ={
+  override def onDestroy(): Unit = {
     super.onDestroy()
     databaseAdapter.close()
   }
--- app/src/main/scala/com/l1/op/activity/RegistrationActivity.scala
+++ app/src/main/scala/com/l1/op/activity/RegistrationActivity.scala
@@ -14,7 +14,7 @@ import com.l1.op.util.{TR, TypedActivity}
 /**
  * Created by Tarun on 4/4/2015.
  */
-class RegistrationActivity extends Activity with TypedActivity{
+class RegistrationActivity extends Activity with TypedActivity {
   lazy val databaseAdapter = new DataBaseAdapter(this)
   //loginDatabaseAdapter.open()
 
@@ -22,25 +22,18 @@ class RegistrationActivity extends Activity with TypedActivity{
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_registration)
 
-    val clickHeader = findViewById(R.id.header).asInstanceOf[ImageView]
-    clickHeader.setOnClickListener(new OnClickListener {
-      override def onClick(v: View): Unit = {
-        val intentHeader: Intent = new Intent()
-        intentHeader.setAction(Intent.ACTION_VIEW)
-        intentHeader.addCategory(Intent.CATEGORY_BROWSABLE)
-        intentHeader.setData(Uri.parse("http://www.l1coaching.co.in"))
-        startActivity(intentHeader)
-      }
-    })
+    //Clickable Header Code
+    findViewById(R.id.header).asInstanceOf[ImageView].onClick {
+      view: View => startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.l1coaching.co.in")))
+    }
 
     val usernameF = findView(TR.usernameEditText)
     val passwordF = findView(TR.passwordEditText)
     val emailF = findView(TR.emailEditText)
     val phoneF = findView(TR.phoneEditText)
 
-    val registerButton = findView(TR.registerButton)
-    registerButton.setOnClickListener(new OnClickListener {
-      override def onClick(v: View): Unit = {
+    findView(TR.registerButton).onClick{
+      view: View =>
         val username = usernameF.getText.toString
         val email = emailF.getText.toString
         val password = passwordF.getText.toString
@@ -51,15 +44,14 @@ class RegistrationActivity extends Activity with TypedActivity{
         } else {
           databaseAdapter.insertUser(username, password, email, "IIT", phone, "0", "0")
           Toast.makeText(getApplicationContext, "Account Successfully Created ", Toast.LENGTH_LONG).show()
-          val intentWelcome :Intent =new Intent(getApplicationContext,classOf[MainActivity])
-          intentWelcome.putExtra("username",username)
+          val intentWelcome: Intent = new Intent(getApplicationContext, classOf[MainActivity])
+          intentWelcome.putExtra("username", username)
           startActivity(intentWelcome)
         }
       }
-
-    })
   }
-  override def onDestroy(): Unit ={
+
+  override def onDestroy(): Unit = {
     super.onDestroy()
     databaseAdapter.close()
   }
